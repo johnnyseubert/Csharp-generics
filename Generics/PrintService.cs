0d5e1d2949e8d5107208dc984632483852254295
DATA53454 <data53454@datainfo.inf.br>
2022-02-24 13:41:15 -0300

    Adicionando arquivos

---
+++ Generics/PrintService.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Generics
+{
+    //Generico é passado o tipo quando for instanciado assim posso utilizar 1 classe para string,int,double etc etc
+    public class PrintService<Generico>
+    {
+        private Generico[] _values = new Generico[10];
+        private int _count = 0;
+         
+        public void AddValue(Generico value)
+        {
+            if (_count == 10)
+            {
+                throw new InvalidOperationException("Print Service is full");
+            }
+            
+            _values[_count] = value;
+            _count++;
+        }
+
+        public Generico First()
+        {
+            if (_count == 0)
+            {
+                throw new InvalidOperationException("Print Service is empty");
+            }
+
+            return _values[0];
+        }
+        
+        public void print(){
+            Console.Write("[");
+            for (int i = 0; i < _count -1; i++)
+            {
+                Console.Write($"{_values[i]}, ");
+            }
+            if (_count > 0 )
+            {
+                Console.Write(_values[_count -1]);
+            }
+            Console.WriteLine("]");
+        }
+    }
+}
---
+++ Generics/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Generics
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            PrintService<int> printService = new PrintService<int>();
+            
+            Console.Write("How many values: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                int x = int.Parse(Console.ReadLine());
+                printService.AddValue(x);
+            }
+            
+            printService.print();
+            Console.WriteLine($"First: {printService.First()}");
+        }
+    }
+}
